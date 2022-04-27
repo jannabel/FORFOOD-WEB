387c6a12aafcb73a7b01aa54360afef3d32f92e1
Jannabel Ramos Ramírez <jannabelramos@gmail.com>
2022-04-27 14:41:40 -0400

    http for users

--- src/app/all_products/products.service.ts
+++ src/app/all_products/products.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpReq } from '../shared/http_req/http-req';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +10,9 @@ export class ProductsService {
 
 constructor(private http: HttpClient) { }
 
-  getProducts() {
-    return this.http.get('/api/Products/Get');
-  }
+
+getProducts() {
+  return this.http.get('/api/Products/Get');
+}
 
 }
